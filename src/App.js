d9f14bfce75d526acc1814b48b21b99cf4f397ab
otfreetime <omaraltaezi@gmail.com>
2022-02-15 23:37:52 +0300

    a

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>THello Word</h1>
+      <h1>Hello Word</h1>
     </div>
   );
 }
