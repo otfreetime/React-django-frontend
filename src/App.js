2b5b4a3ba9e42b237f37f60bcbf168c2d2582319
otfreetime <omaraltaezi@gmail.com>
2022-02-15 23:34:53 +0300

    comit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        Hello Wold
-        </p>
-        <a
-          className="Hello"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>THello Word</h1>
     </div>
   );
 }
